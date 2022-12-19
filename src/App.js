1ff225c5e13a34530fb8c9e8c3d2361aa1bb4743
balajisaikumar2000 <73514569+balajisaikumar2000@users.noreply.github.com>
2022-12-19 19:39:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { useState } from 'react';
-import Grid from './components/Grid'
+// import Grid from './components/Grid'
 import ShowGrid from './components/SingleGridComponent';
 
 
 function App() {
 
-  const [grid, setGrid] = useState(new Array(32).fill(0).map(() => new Array(32).fill(0)));
+  const [grid] = useState(new Array(32).fill(0).map(() => new Array(32).fill(0)));
 
   return (
       <ShowGrid grid={grid}></ShowGrid>
